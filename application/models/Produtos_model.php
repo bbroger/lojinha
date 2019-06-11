8d8285cc12cf638e3722a02a40ba68f810de5916
Goodnato <renatoalves_nascimento@hotmail.com>
2019-06-11 11:08:13 -0300

    Configuração pronto

---
+++ application/models/Produtos_model.php
@@ -0,0 +1,28 @@
+<?php
+
+class Produtos_model extends CI_Model
+{
+    /*
+    $query = $this->db->get_where('audio', ['id_company' => $id_company, 'status' => 'active']);
+
+        return $query->result_array();
+
+    $this->db->select('list.*, campaing.name AS cname')
+            ->from('list')
+            ->join('campaing', 'campaing.id_list LIKE concat("%",list.id_list,"%")', 'left')
+            ->order_by('list.id_list', 'ASC');
+        $query = $this->db->get();
+
+        return $query->result_array();
+
+    $data = ['id_company' => $id_company, 'name' => $name];
+
+        $this->db->insert('list', $data);
+        $insert_id = $this->db->insert_id();
+
+        return  $insert_id;
+    
+    $data[coluna do banco]= valor;
+        $this->db->insert_batch('list_value', $data);
+    */
+}
