0ff13661ef01cc62a4ed39b3f4832b1e0cc494ae
Goodnato <renatoalves_nascimento@hotmail.com>
2019-07-08 01:19:04 -0300

    Terminei o FRONT dos relatorios

---
+++ application/views/relatorios.php
@@ -0,0 +1,173 @@
+<div class="container-fluid">
+    <div class="row mt-4">
+        <div class="col-12" style="background-color: rgba(255,255,255,0.4)">
+            <div class="row">
+                <div class="col-2">
+                    <nav class="nav flex-column">
+                        <button class="btn btn-outline-dark btn-lg active" href="#">Vendas</button>
+
+                        <button class="btn btn-outline-primary  btn-lg mt-5" href="#">Dinheiro</button>
+
+                        <button class="btn btn-outline-success btn-lg mt-5" href="#">Cartão</button>
+                    </nav>
+                </div>
+                <div class="col-10">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <canvas id="graphMonth" style="height: 380px;"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <canvas id="graphTotalTrans" style="height: 300px;"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <canvas id="graphTotalVendas" style="height: 300px;"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-6" style="padding-right: 0;">
+                                            <div id="circlefulMoney" style="height: 150px; width: 150px; margin: 0 auto;"></div>
+                                        </div>
+                                        <div class="col-6" style="padding-left: 0;">
+                                            <div id="circlefulCard" style="height: 150px; width: 150px; margin: 0 auto;"></div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-6" style="padding-right: 0;">
+                                            <div id="circlefulAtacado" style="height: 150px; width: 150px; margin: 0 auto;"></div>
+                                        </div>
+                                        <div class="col-6" style="padding-left: 0;">
+                                            <div id="circlefulVarejo" style="height: 150px; width: 150px; margin: 0 auto;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col-4">
+                            <table class="table table-striped text-center" id="mostra_tabela">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th scope="col">Data</th>
+                                        <th scope="col">Valor</th>
+                                        <th scope="col">Itens</th>
+                                        <th scope="col">Ação</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>20/03 13:00</td>
+                                        <td>R$ 102.43</td>
+                                        <td>10</td>
+                                        <td>Ver</td>
+                                    </tr>
+                                    <tr>
+                                        <td>20/03 13:00</td>
+                                        <td>R$ 102.43</td>
+                                        <td>10</td>
+                                        <td>Ver</td>
+                                    </tr>
+                                    <tr>
+                                        <td>20/03 13:00</td>
+                                        <td>R$ 102.43</td>
+                                        <td>10</td>
+                                        <td>Ver</td>
+                                    </tr>
+                                    <tr>
+                                        <td>20/03 13:00</td>
+                                        <td>R$ 102.43</td>
+                                        <td>10</td>
+                                        <td>Ver</td>
+                                    </tr>
+                                    <tr>
+                                        <td>20/03 13:00</td>
+                                        <td>R$ 102.43</td>
+                                        <td>10</td>
+                                        <td>Ver</td>
+                                    </tr>
+                                    <tr>
+                                        <td>20/03 13:00</td>
+                                        <td>R$ 102.43</td>
+                                        <td>10</td>
+                                        <td>Ver</td>
+                                    </tr>
+                                    <tr>
+                                        <td>20/03 13:00</td>
+                                        <td>R$ 102.43</td>
+                                        <td>10</td>
+                                        <td>Ver</td>
+                                    </tr>
+                                    <tr>
+                                        <td>20/03 13:00</td>
+                                        <td>R$ 102.43</td>
+                                        <td>10</td>
+                                        <td>Ver</td>
+                                    </tr>
+                                    <tr>
+                                        <td>20/03 13:00</td>
+                                        <td>R$ 102.43</td>
+                                        <td>10</td>
+                                        <td>Ver</td>
+                                    </tr>
+                                    <tr>
+                                        <td>20/03 13:00</td>
+                                        <td>R$ 102.43</td>
+                                        <td>10</td>
+                                        <td>Ver</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="col-8">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <canvas id="graphWeek" style="height: 300px;"></canvas>
+                                        </div>
+                                        <div class="col-6">
+                                            <canvas id="graphWeekTotalTrans" style="height: 280px;"></canvas>
+                                        </div>
+                                        <div class="col-6">
+                                            <canvas id="graphWeekTotalVendas" style="height: 280px;"></canvas>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="<?php echo base_url("assets/js/jquery.min.js"); ?>"></script>
+<script src="<?php echo base_url("assets/js/popper.min.js"); ?>"></script>
+<script src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
+<script src="<?php echo base_url("assets/js/moment.min.js"); ?>"></script>
+<script src="<?php echo base_url("assets/js/dataTables.min.js"); ?>"></script>
+<script src="<?php echo base_url("assets/js/dataTables.bootstrap4.min.js"); ?>"></script>
+<script src="<?php echo base_url("assets/js/maskMoney.min.js"); ?>"></script>
+<script src="<?php echo base_url("assets/js/chartjs.min.js"); ?>"></script>
+<script src="<?php echo base_url("assets/js/circliful.min.js"); ?>"></script>
+<script src="<?php echo base_url("assets/js/relatorios.js"); ?>"></script>
+</body>
+
+</html>
---
+++ assets/js/relatorios.js
@@ -0,0 +1,318 @@
+var idMounth = $("#graphMonth");
+var chartMounth = new Chart(idMounth, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                fill: false,
+                type: 'line',
+                label: 'Total',
+                backgroundColor: 'blue',
+                borderColor: 'blue',
+                data: [50, 43, 67, 86, 37, 70, 45]
+            },
+            {
+                type: 'bar',
+                label: 'Atacado',
+                borderColor: 'white',
+                borderWidth: 2,
+                backgroundColor: 'yellow',
+                data: [0, 13, 5, 42, 23, 5, 32]
+            },
+            {
+                type: 'bar',
+                label: 'Varejo',
+                borderColor: 'white',
+                borderWidth: 2,
+                backgroundColor: 'green',
+                data: [32, 32, 45, 13, 21, 50, 7]
+            },
+            {
+                fill: false,
+                type: 'bar',
+                label: 'Desconto',
+                borderColor: 'white',
+                backgroundColor: 'red',
+                borderWidth: 2,
+                data: [0, 0, 5, 8, 0, 7, 5]
+            },
+        ]
+    },
+
+    // Configuration options go here
+    options: {
+        title: {
+            display: true,
+            text: 'Vendas mensal',
+            position: 'top'
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    callback: function (value, index, values) {
+                        return 'R$' + value;
+                    }
+                }
+            }]
+        },
+        animation: {
+            duration: 5000
+        },
+        maintainAspectRatio: false,
+    }
+
+});
+
+var idTotalTrans = $("#graphTotalTrans");
+var chartTotalTrans = new Chart(idTotalTrans, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [21, 32, 5, 32, 53, 4, 7],
+            backgroundColor: ['red', 'orange', 'yellow', 'green', 'blue', 'black', 'pink'],
+            labels:['January', 'February', 'March', 'April', 'May', 'June', 'July']
+        }]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'none',
+        },
+        title: {
+            display: true,
+            text: 'Total de transação'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        },
+        animation: {
+            duration: 5000
+        },
+        maintainAspectRatio: false
+    }
+});
+
+var idTotalVendas = $("#graphTotalVendas");
+var chartTotalVendas = new Chart(idTotalVendas, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [21, 32, 5, 32, 53, 4, 7],
+            backgroundColor: ['red', 'orange', 'yellow', 'green', 'blue', 'black', 'pink']
+        }],
+        labels:['January', 'February', 'March', 'April', 'May', 'June', 'July']
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'none',
+        },
+        title: {
+            display: true,
+            text: 'Total de produtos vendidos'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true,
+            duration: 5000
+        },
+        maintainAspectRatio: false
+    }
+});
+
+$("#circlefulMoney").circliful({
+    animationStep: 3,
+    foregroundBorderWidth: 20,
+    backgroundBorderWidth: 20,
+    percent: 75,
+    text: 'Dinheiro',
+    textY: 85
+});
+
+$("#circlefulCard").circliful({
+    animationStep: 3,
+    foregroundBorderWidth: 20,
+    backgroundBorderWidth: 20,
+    percent: 20,
+    text: 'Cartão',
+    textY: 85
+});
+
+$("#circlefulAtacado").circliful({
+    animationStep: 3,
+    foregroundBorderWidth: 20,
+    backgroundBorderWidth: 20,
+    percent: 75,
+    text: 'Atacado',
+    textY: 85
+});
+
+$("#circlefulVarejo").circliful({
+    animationStep: 3,
+    foregroundBorderWidth: 20,
+    backgroundBorderWidth: 20,
+    percent: 20,
+    text: 'Varejo',
+    textY: 85
+});
+
+var table= $("#mostra_tabela").DataTable({
+    "info": false,
+    "ordering": false,
+    "dom": "ftip",
+    "language": {
+        "sEmptyTable": "Nenhum registro encontrado",
+        "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+        "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+        "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+        "sInfoPostFix": "",
+        "sInfoThousands": ".",
+        "sLengthMenu": "_MENU_ resultados por página",
+        "sLoadingRecords": "Carregando...",
+        "sProcessing": "Processando...",
+        "sZeroRecords": "Nenhum registro encontrado",
+        "sSearch": "Pesquisar",
+        "oPaginate": {
+            "sNext": "Próximo",
+            "sPrevious": "Anterior",
+            "sFirst": "Primeiro",
+            "sLast": "Último"
+        },
+        "oAria": {
+            "sSortAscending": ": Ordenar colunas de forma ascendente",
+            "sSortDescending": ": Ordenar colunas de forma descendente"
+        }
+    }
+});
+
+var idWeek = $("#graphWeek");
+var chartWeek = new Chart(idWeek, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                fill: false,
+                type: 'line',
+                label: 'Total',
+                backgroundColor: 'blue',
+                borderColor: 'blue',
+                data: [50, 43, 67, 86, 37, 70, 45]
+            },
+            {
+                type: 'bar',
+                label: 'Atacado',
+                borderColor: 'white',
+                borderWidth: 2,
+                backgroundColor: 'yellow',
+                data: [0, 13, 5, 42, 23, 5, 32]
+            },
+            {
+                type: 'bar',
+                label: 'Varejo',
+                borderColor: 'white',
+                borderWidth: 2,
+                backgroundColor: 'green',
+                data: [32, 32, 45, 13, 21, 50, 7]
+            },
+            {
+                fill: false,
+                type: 'bar',
+                label: 'Desconto',
+                borderColor: 'white',
+                backgroundColor: 'red',
+                borderWidth: 2,
+                data: [0, 0, 5, 8, 0, 7, 5]
+            },
+        ]
+    },
+
+    // Configuration options go here
+    options: {
+        title: {
+            display: true,
+            text: 'Vendas semanal',
+            position: 'top'
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    callback: function (value, index, values) {
+                        return 'R$' + value;
+                    }
+                }
+            }]
+        },
+        animation: {
+            duration: 5000
+        },
+        maintainAspectRatio: false,
+    }
+
+});
+
+var idWeekTotalTrans = $("#graphWeekTotalTrans");
+var chartWeekTotalTrans = new Chart(idWeekTotalTrans, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [21, 32, 5, 32, 53, 4, 7],
+            backgroundColor: ['red', 'orange', 'yellow', 'green', 'blue', 'black', 'pink'],
+            labels:['January', 'February', 'March', 'April', 'May', 'June', 'July']
+        }]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'none',
+        },
+        title: {
+            display: true,
+            text: 'Total de transação'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        },
+        animation: {
+            duration: 5000
+        },
+        maintainAspectRatio: false
+    }
+});
+
+var idWeekTotalVendas = $("#graphWeekTotalVendas");
+var chartWeekTotalVendas = new Chart(idWeekTotalVendas, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [21, 32, 5, 32, 53, 4, 7],
+            backgroundColor: ['red', 'orange', 'yellow', 'green', 'blue', 'black', 'pink']
+        }],
+        labels:['January', 'February', 'March', 'April', 'May', 'June', 'July']
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'none',
+        },
+        title: {
+            display: true,
+            text: 'Total de produtos vendidos'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true,
+            duration: 5000
+        },
+        maintainAspectRatio: false
+    }
+});
