b2c1d82ebb41b21e348eb5daf0affadf976b151f
Goodnato <renatoalves_nascimento@hotmail.com>
2019-07-10 01:48:31 -0300

    Adicionei ano nas consultas e terminei o circli and total transacao

--- application/controllers/Relatorios.php
+++ application/controllers/Relatorios.php
@@ -22,15 +22,11 @@ class Relatorios extends CI_Controller
     public function vendas_mensal()
     {
         $dados = $this->Relatorios_model->vendas_mensal();
-        
+
         foreach ($dados as $key => $value) {
             foreach ($value as $chave => $valor) {
                 if ($chave != 'mes' && $valor) {
-                    if ($chave == 'valor_total') {
-                        $arr[$value['mes']][$chave] = $valor;
-                    } else {
-                        $arr[$value['mes']][$chave] = $valor;
-                    }
+                    $arr[$value['mes']][$chave] = $valor;
                 } else if ($chave != 'mes' && !isset($arr[$value['mes']][$chave])) {
                     $arr[$value['mes']][$chave] = 0;
                 }
@@ -55,8 +51,8 @@ class Relatorios extends CI_Controller
             }
             $count++;
         }
-        
-        $label_meses= [];
+
+        $label_meses = [];
         foreach ($arr as $key => $value) {
             $count = 0;
             array_push($label_meses, $meses[$key]);
@@ -75,7 +71,68 @@ class Relatorios extends CI_Controller
                 $count++;
             }
         }
-        
-        return ["label_meses"=> $label_meses, "data"=>$vendas_mensal];
+
+        return ["label_meses" => $label_meses, "data" => $vendas_mensal];
+    }
+
+    public function transacao()
+    {
+        $dados = $this->Relatorios_model->transacao();
+
+        foreach ($dados as $key => $value) {
+            foreach ($value as $chave => $valor) {
+                if ($chave != 'mes' && $valor) {
+                    $arr[$value['mes']][$chave] = ($valor) ? $valor : 0;
+                } else if ($chave != 'mes' && !isset($arr[$value['mes']][$chave])) {
+                    $arr[$value['mes']][$chave] = 0;
+                }
+            }
+        }
+
+        $meses = [1 => 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+        $cores = [
+            1 => 'rgb(255, 99, 132)', 'rgb(255, 159, 64)', 'rgb(255, 205, 86)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)', 'rgb(176,196,222)', 'rgb(220,20,60)', 'rgb(255,20,147)', 'rgb(255,140,0)', 'rgb(210,105,30)'
+        ];
+
+        $labels_meses = [];
+        $colors_meses = [];
+        foreach ($arr as $key => $value) {
+            array_push($labels_meses, $meses[$key]);
+            array_push($colors_meses, $cores[$key]);
+        }
+
+        $total_transacao['data'] = array_column($arr, 'total_transacao');
+        $total_transacao['backgroundColor'] = $colors_meses;
+        $total_transacao['labels'] = $labels_meses;
+
+        $circli = $this->circli($arr);
+
+        return ["tota_transacao" => $total_transacao, "circli" => $circli];
+    }
+
+    public function circli($arr)
+    {
+        $soma_total_transacao = array_sum(array_column($arr, 'total_transacao'));
+        $soma_dinheiro = array_sum(array_column($arr, 'total_dinheiro'));
+        $soma_cartao = array_sum(array_column($arr, 'total_cartao'));
+        $soma_atacado = array_sum(array_column($arr, 'total_atacado'));
+        $soma_varejo = array_sum(array_column($arr, 'total_varejo'));
+
+        $porc_dinheiro = $soma_dinheiro / $soma_total_transacao * 100;
+        $porc_cartao = $soma_cartao / $soma_total_transacao * 100;
+        $porc_atacado = $soma_atacado / $soma_total_transacao * 100;
+        $porc_varejo = $soma_varejo / $soma_total_transacao * 100;
+
+        $circli['dinheiro']['porc'] = (strpos($porc_dinheiro, '.') === false) ? (int) $porc_dinheiro : (float) number_format($porc_dinheiro, 1, '.', '');
+        $circli['dinheiro']['decimal'] = (is_int($circli['dinheiro']['porc'])) ? 0 : 1;
+        $circli['cartao']['porc'] = (strpos($porc_cartao, '.') === false) ? (int) $porc_cartao : (float) number_format($porc_cartao, 1, '.', '');
+        $circli['cartao']['decimal'] = (is_int($circli['cartao']['porc'])) ? 0 : 1;
+        $circli['atacado']['porc'] = (strpos($porc_atacado, '.') === false) ? (int) $porc_atacado : (float) number_format($porc_atacado, 1, '.', '');
+        $circli['atacado']['decimal'] = (is_int($circli['atacado']['porc'])) ? 0 : 1;
+        $circli['varejo']['porc'] = (strpos($porc_varejo, '.') === false) ? (int) $porc_varejo : (float) number_format($porc_varejo, 1, '.', '');
+        $circli['varejo']['decimal'] = (is_int($circli['varejo']['porc'])) ? 0 : 1;
+
+        return $circli;
     }
 }
--- application/models/Relatorios_model.php
+++ application/models/Relatorios_model.php
@@ -4,6 +4,7 @@ class Relatorios_model extends CI_Model
 {
     public function vendas_mensal()
     {
+        $ano= date('Y');
         $sql = "SELECT DATE_FORMAT(timestamp, '%c') AS mes, 
             SUM(valor_total) - SUM(desconto) AS valor_total, 
             NULL AS valor_atacado, 
@@ -11,7 +12,7 @@ class Relatorios_model extends CI_Model
             NULL AS valor_desconto, 
             NULL AS valor_retirado, 
             NULL AS valor_inserido 
-            FROM transacao GROUP BY mes 
+            FROM transacao WHERE YEAR(timestamp) = $ano GROUP BY mes 
             UNION ALL 
             SELECT DATE_FORMAT(timestamp, '%c') AS mes, 
             NULL AS valor_total, 
@@ -20,7 +21,7 @@ class Relatorios_model extends CI_Model
             NULL AS valor_desconto, 
             NULL AS valor_retirado, 
             NULL AS valor_inserido 
-            FROM transacao WHERE venda= 'atacado' GROUP BY mes 
+            FROM transacao WHERE venda= 'atacado' AND YEAR(timestamp) = $ano GROUP BY mes 
             UNION ALL 
             SELECT DATE_FORMAT(timestamp, '%c') AS mes, 
             NULL AS valor_total, 
@@ -29,7 +30,7 @@ class Relatorios_model extends CI_Model
             NULL AS valor_desconto, 
             NULL AS valor_retirado, 
             NULL AS valor_inserido 
-            FROM transacao WHERE venda= 'varejo' GROUP BY mes 
+            FROM transacao WHERE venda= 'varejo' AND YEAR(timestamp) = $ano GROUP BY mes 
             UNION ALL 
             SELECT DATE_FORMAT(timestamp, '%c') AS mes, 
             NULL AS valor_total, 
@@ -38,7 +39,7 @@ class Relatorios_model extends CI_Model
             SUM(desconto) AS valor_desconto, 
             NULL AS valor_retirado, 
             NULL AS valor_inserido 
-            FROM transacao GROUP BY mes 
+            FROM transacao WHERE YEAR(timestamp) = $ano GROUP BY mes 
             UNION ALL 
             SELECT DATE_FORMAT(timestamp, '%c') AS mes, 
             NULL AS valor_total, 
@@ -47,7 +48,7 @@ class Relatorios_model extends CI_Model
             NULL AS valor_desconto, 
             SUM(valor) AS valor_retirado, 
             NULL AS valor_inserido 
-            FROM movimentacao WHERE tipo= 'retirado' GROUP BY mes 
+            FROM movimentacao WHERE tipo= 'retirado' AND YEAR(timestamp) = $ano GROUP BY mes 
             UNION ALL 
             SELECT DATE_FORMAT(timestamp, '%c') AS mes, 
             NULL AS valor_total, 
@@ -56,7 +57,54 @@ class Relatorios_model extends CI_Model
             NULL AS valor_desconto, 
             NULL AS valor_retirado, 
             SUM(valor) AS valor_inserido 
-            FROM movimentacao WHERE tipo= 'inserido' GROUP BY mes";
+            FROM movimentacao WHERE tipo= 'inserido' AND YEAR(timestamp) = $ano GROUP BY mes";
+
+        $query = $this->db->query($sql);
+        return $query->result_array();
+    }
+    
+    public function transacao()
+    {
+        $ano= date('Y');
+        $sql = "SELECT DATE_FORMAT(timestamp, '%c') AS mes, 
+            COUNT(id_transacao) AS total_transacao, 
+            NULL AS total_atacado, 
+            NULL AS total_varejo, 
+            NULL AS total_dinheiro, 
+            NULL AS total_cartao 
+            FROM transacao WHERE YEAR(timestamp) = $ano GROUP BY mes 
+            UNION ALL 
+            SELECT DATE_FORMAT(timestamp, '%c') AS mes, 
+            NULL AS total_transacao, 
+            COUNT(id_transacao) AS total_atacado, 
+            NULL AS total_varejo, 
+            NULL AS total_dinheiro, 
+            NULL AS total_cartao 
+            FROM transacao WHERE venda= 'atacado' AND YEAR(timestamp) = $ano GROUP BY mes 
+            UNION ALL 
+            SELECT DATE_FORMAT(timestamp, '%c') AS mes, 
+            NULL AS total_transacao, 
+            NULL AS total_atacado, 
+            COUNT(id_transacao) AS total_varejo, 
+            NULL AS total_dinheiro, 
+            NULL AS total_cartao 
+            FROM transacao WHERE venda= 'varejo' AND YEAR(timestamp) = $ano GROUP BY mes 
+            UNION ALL 
+            SELECT DATE_FORMAT(timestamp, '%c') AS mes, 
+            NULL AS total_transacao, 
+            NULL AS total_atacado, 
+            NULL AS total_varejo, 
+            COUNT(id_transacao) AS total_dinheiro, 
+            NULL AS total_cartao 
+            FROM transacao WHERE tipo_pagamento= 'dinheiro' AND YEAR(timestamp) = $ano GROUP BY mes 
+            UNION ALL 
+            SELECT DATE_FORMAT(timestamp, '%c') AS mes, 
+            NULL AS total_transacao, 
+            NULL AS total_atacado, 
+            NULL AS total_varejo, 
+            NULL AS total_dinheiro, 
+            COUNT(id_transacao) AS total_cartao 
+            FROM transacao WHERE tipo_pagamento= 'cartao' AND YEAR(timestamp) = $ano GROUP BY mes";
 
         $query = $this->db->query($sql);
         return $query->result_array();
